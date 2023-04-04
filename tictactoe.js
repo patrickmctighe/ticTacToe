8578c76723da508ee3e3d11bae0800af61606273
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-04-04 14:49:20 -0400

    functionality-getting-clode-to-done

--- tictactoe.js
+++ tictactoe.js
@@ -1,18 +1,17 @@
 let gameBoard = (() => {
-  return {
-    cells: [
-      [1, Math.random() * 7, 1],
-      [2, Math.random() * 7, 2],
-      [3, Math.random() * 7, 3],
-      [4, Math.random() * 7, 4],
-      [5, Math.random() * 7, 5],
-      [6, Math.random() * 7, 6],
-      [7, Math.random() * 7, 7],
-      [8, Math.random() * 7, 8],
-      [9, Math.random() * 7, 9],
-    ],
-
-    players: {
+    let cells = [
+      [1, "a1"],
+      [2, "a2"],
+      [3, "a3"],
+      [4, "b1"],
+      [5, "b2"],
+      [6, "b3"],
+      [7, "c1"],
+      [8, "c2"],
+      [9, "c3"],
+    ];
+  
+    let players = {
       player1: {
         name: "player one",
         shape: "X",
@@ -21,83 +20,87 @@ let gameBoard = (() => {
         name: "player two",
         shape: "O",
       },
-    },
-    container: document.getElementById("boardArea"),
-    squares: document.createElement("button"),
-    p1Board: [],
-    p2Board: [],
-    matchedP1: [],
-    matchedP2:[],
-    winningNums: [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 4, 7],
-      [2, 5, 7],
-      [3, 6, 9],
-      [1, 5, 9],
-      [3, 5, 7],
-    ],
-  };
-})();
-
-function makeboard(array) {
-  array.forEach(([item, data, name]) => {
-    // console.log(data);
-    let squares = document.createElement("button");
-    squares.setAttribute("class", item);
-    squares.setAttribute("id", "squares");
-    squares.setAttribute("value", data);
-    squares.setAttribute("name", name);
-    gameBoard.container.append(squares);
-    return squares;
-  });
-}
-
-function makeMoves() {
-  let playNum = 1;
-  gameBoard.container.addEventListener("click", function makeP1Move(e) {
-    if (
-      e.target.innerText !== gameBoard.players.player2.shape &&
-      e.target.innerText !== gameBoard.players.player1.shape
-    ) {
-      if (playNum % 2 == 0) {
-        e.target.innerText = gameBoard.players.player2.shape;
-        playNum++;
-        gameBoard.p2Board.push(e.target.name && gameBoard.players.player2.shape);
-        // console.log(gameBoard.p2Board);
-        // winListP2(gameBoard.p2Board, gameBoard.winningNums);
-        // console.log(playNum);
-      } else if (playNum % 2 !== 0) {
-        e.target.innerText = gameBoard.players.player1.shape;
-        playNum++;
-        gameBoard.p1Board.push(e.target.name && gameBoard.players.player1.shape);
-        // console.log(gameBoard.p1Board);
-        winListP1();
-        // console.log(playNum);
+    };
+  
+    let container = document.getElementById("boardArea"); 
+    let winningNums = [
+      ["a1", "a2", "a3"],
+      ["b1", "b2", "b3"],
+      ["c1", "c2", "c3"],
+      ["a1", "b1", "c1"],
+      ["a2", "b2", "c2"],
+      ["a3", "b3", "c3"],
+      ["a1", "b2", "c3"],
+      ["a3", "b2", "c1"],
+    ];
+    let board = [];
+    let p1Moves = [];
+    let p2Moves = [];
+  
+    return {
+      cells,
+      players,
+      container, 
+      winningNums,
+      board,
+      p1Moves,
+      p2Moves,
+    };
+  })();
+  
+  function makeboard(array) {
+    array.forEach(([item, name]) => {
+      // console.log(data);
+      let squares = document.createElement("button");
+      squares.setAttribute("class", item);
+      squares.setAttribute("id", "squares");
+      squares.setAttribute("name", name);
+      gameBoard.container.append(squares);
+      return squares;
+    });
+  }
+  
+  function makeMoves() {
+    let playNum = 1;
+    gameBoard.container.addEventListener("click", function makeMove(e) {
+      if (
+        e.target.innerText !== gameBoard.players.player2.shape &&
+        e.target.innerText !== gameBoard.players.player1.shape
+      ) {
+        if (playNum % 2 == 0) {
+          e.target.innerText = gameBoard.players.player2.shape;
+          playNum++;
+          gameBoard.p2Moves.push(e.target.name);
+          if (checkPlayerMovesForWin(gameBoard.p2Moves, gameBoard.winningNums)) { 
+            console.log(`${gameBoard.players.player2.name} wins!`);
+          }
+        } else if (playNum % 2 !== 0) {
+          e.target.innerText = gameBoard.players.player1.shape;
+          playNum++;
+          gameBoard.p1Moves.push(e.target.name);
+          if (checkPlayerMovesForWin(gameBoard.p1Moves, gameBoard.winningNums)) { 
+            console.log(`${gameBoard.players.player1.name} wins!`);
+          }
+        }
+      }
+    });
+  }
+  
+  function checkPlayerMovesForWin(pMoves, winningNums) {
+    for (let i = 0; i < winningNums.length; i++) {
+      let winner = true;
+      for (let j = 0; j < winningNums[i].length; j++) {
+        if (pMoves.indexOf(winningNums[i][j]) === -1) {
+          winner = false;
+          break;
+        }
+      }
+      if (winner) {
+        return true;
       }
     }
-  });
-}
-// all the funtions ive tried so far
-function winListP1(){
-    for (let i = 0; i < gameBoard.winningNums.length; i++){
-        if(gameBoard.p1Board.includes(gameBoard.winningNums.indexOf(i))){
-         console.log("yey")
-        } else { console.log("dang")}
-    }
-}
-
-
-
-
-
-
-let clear = document.querySelector(".clear");
-clear.addEventListener("click", clearBoard());
-function clearBoard() {
-  gameBoard.squares.innerHTML = "";
-}
-
-makeMoves();
-makeboard(gameBoard.cells);
+    return false;
+  }
+  
+  makeMoves();
+  makeboard(gameBoard.cells);
