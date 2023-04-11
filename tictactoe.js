9a4232cc5307ee7a63d508db2a235ce85efbf371
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-04-11 12:23:37 -0400

    finished-will-try-making-a-ai-option-later

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,6 @@
 let gameBoard = (() => {
-    let cells = [
+  return {
+    cells: [
       [1, "a1"],
       [2, "a2"],
       [3, "a3"],
@@ -9,9 +10,9 @@ let gameBoard = (() => {
       [7, "c1"],
       [8, "c2"],
       [9, "c3"],
-    ];
-  
-    let players = {
+    ],
+
+    players: {
       player1: {
         name: "player one",
         shape: "X",
@@ -20,10 +21,15 @@ let gameBoard = (() => {
         name: "player two",
         shape: "O",
       },
-    };
-  
-    let container = document.getElementById("boardArea"); 
-    let winningNums = [
+    },
+    container: document.getElementById("boardArea"),
+    squares: document.createElement("button"),
+    board: [],
+    p1Moves: [],
+    p2Moves: [],
+    p1Pts: 0,
+    p2Pts: 0,
+    winningNums: [
       ["a1", "a2", "a3"],
       ["b1", "b2", "b3"],
       ["c1", "c2", "c3"],
@@ -32,75 +38,122 @@ let gameBoard = (() => {
       ["a3", "b3", "c3"],
       ["a1", "b2", "c3"],
       ["a3", "b2", "c1"],
-    ];
-    let board = [];
-    let p1Moves = [];
-    let p2Moves = [];
-  
-    return {
-      cells,
-      players,
-      container, 
-      winningNums,
-      board,
-      p1Moves,
-      p2Moves,
-    };
-  })();
-  
-  function makeboard(array) {
-    array.forEach(([item, name]) => {
-      // console.log(data);
-      let squares = document.createElement("button");
-      squares.setAttribute("class", item);
-      squares.setAttribute("id", "squares");
-      squares.setAttribute("name", name);
-      gameBoard.container.append(squares);
-      return squares;
-    });
-  }
-  
-  function makeMoves() {
-    let playNum = 1;
-    gameBoard.container.addEventListener("click", function makeMove(e) {
-      if (
-        e.target.innerText !== gameBoard.players.player2.shape &&
-        e.target.innerText !== gameBoard.players.player1.shape
-      ) {
-        if (playNum % 2 == 0) {
-          e.target.innerText = gameBoard.players.player2.shape;
-          playNum++;
-          gameBoard.p2Moves.push(e.target.name);
-          if (checkPlayerMovesForWin(gameBoard.p2Moves, gameBoard.winningNums)) { 
-            console.log(`${gameBoard.players.player2.name} wins!`);
-          }
-        } else if (playNum % 2 !== 0) {
-          e.target.innerText = gameBoard.players.player1.shape;
-          playNum++;
-          gameBoard.p1Moves.push(e.target.name);
-          if (checkPlayerMovesForWin(gameBoard.p1Moves, gameBoard.winningNums)) { 
-            console.log(`${gameBoard.players.player1.name} wins!`);
-          }
+    ],
+  };
+})();
+
+let p1Name = document.querySelector(".playerOneName");
+let p2Name = document.querySelector(".playerTwoName");
+let winText = document.querySelector(".youWin");
+function makeboard(array) {
+  array.forEach(([item, name]) => {
+    // console.log(data);
+    let squares = document.createElement("button");
+    squares.setAttribute("class", item);
+    squares.setAttribute("id", "squares");
+    squares.setAttribute("name", name);
+    gameBoard.container.append(squares);
+    return squares;
+  });
+}
+
+function makeMoves() {
+  let playNum = 1;
+  gameBoard.container.addEventListener("click", function makeP1Move(e) {
+    if (
+      e.target.innerText !== gameBoard.players.player2.shape &&
+      e.target.innerText !== gameBoard.players.player1.shape
+    ) {
+      if (playNum % 2 == 0) {
+        e.target.innerText = gameBoard.players.player2.shape;
+        playNum++;
+        gameBoard.p2Moves.push(e.target.name);
+        gameBoard.board.push([e.target.name, gameBoard.players.player2.shape]);
+        if (checkPlayerMovesForWin(gameBoard.p2Moves, gameBoard.winningNums)) {
+          winText.innerHTML = ` ${p2Name.innerHTML} is the Winner!!`;
+          playNum = 1;
+          updateScore("player2");
+        }
+      } else if (playNum % 2 !== 0) {
+        e.target.innerText = gameBoard.players.player1.shape;
+        playNum++;
+        gameBoard.p1Moves.push(e.target.name);
+        gameBoard.board.push([e.target.name, gameBoard.players.player2.shape]);
+        if (checkPlayerMovesForWin(gameBoard.p1Moves, gameBoard.winningNums)) {
+          winText.innerHTML = `${p1Name.innerHTML} is the Winner!!`;
+          playNum = 1;
+          updateScore("player1");
+          console.log(gameBoard.p1Moves);
         }
       }
-    });
-  }
-  
-  function checkPlayerMovesForWin(pMoves, winningNums) {
-    for (let i = 0; i < winningNums.length; i++) {
-      let winner = true;
-      for (let j = 0; j < winningNums[i].length; j++) {
-        if (pMoves.indexOf(winningNums[i][j]) === -1) {
-          winner = false;
-          break;
-        }
-      }
-      if (winner) {
-        return true;
-      }
+      if( playNum == 10){
+        winText.innerHTML = `It's a tie!!`;
+      }console.log(playNum)
     }
-    return false;
+  });
+}
+function updateScore(player) {
+  let p1Score = document.querySelector(".playerOneScore");
+  let p2Score = document.querySelector(".playerTwoScore");
+  if (player === "player1") {
+    gameBoard.p1Pts++;
+    p1Score.innerHTML = `Score: ${gameBoard.p1Pts}`;
+  } else {
+    gameBoard.p2Pts++;
+    p2Score.innerHTML = `Score: ${gameBoard.p2Pts}`;
   }
+}
+
+const checkPlayerMovesForWin = (pMoves, winningNums) => {
+  return winningNums.some((winningCombination) => {
+    return winningCombination.every((square) => pMoves.includes(square));
+  });
+};
+
+console.log(gameBoard.p1Moves);
+let resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  resetBoard();
+});
+function resetBoard() {
+  let squares = document.querySelectorAll("#squares");
+  let winText = document.querySelector(".youWin");
+  squares.forEach((square) => {
+    square.innerHTML = "";
+    gameBoard.p1Moves = [];
+    gameBoard.p2Moves = [];
+    winText.innerHTML = "";
+  });
+}
+
+document.querySelector(".popForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const form = document.querySelector(".popForm");
+  let player1Name = form.elements[0].value;
+  let player2Name = form.elements[1].value;
+
+  if (player1Name != "") {
+    let p1Name = document.querySelector(".playerOneName");
+    p1Name.innerHTML = `${player1Name}`;
+  } 
   
-  makeMoves();
-  makeboard(gameBoard.cells);
+  if (player2Name != "") {
+    let p2Name = document.querySelector(".playerTwoName");
+    p2Name.innerHTML = `${player2Name}`;
+  } 
+});
+
+const popUp = document.querySelector(".pop-up");
+const startBtn = document.querySelector(".btn");
+const gameContainer = document.getElementById("gameContainer");
+// Show the pop-up on page load
+popUp.style.display = "block";
+
+startBtn.addEventListener("click", () => {
+  popUp.style.display = "none";
+  gameContainer.style.display = "block";
+});
+
+makeMoves();
+makeboard(gameBoard.cells);
